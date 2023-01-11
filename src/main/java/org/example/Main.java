652f0a75c2626883b5ae6723a6ae8acc6a37a91b
Nidhi Agrawal <nidhi.agrawal@jobtwine.com>
2023-01-11 22:25:06 +0530

    clears captcha page

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver.exe"); // path of chromedriver
         //opening chrome and then eprocurement page
        ChromeDriver driver=new ChromeDriver();
        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
@@ -19,7 +21,17 @@ public class Main {
         // Launching website
         driver.get("https://eproc.cgstate.gov.in/CHEPS/security/getSignInAction.do");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        WebElement element=driver.findElement(By.name("Advanced Search"));
-        element.click();
+        List<WebElement> element=driver.findElements(By.className("epsAdvSearchBtn"));
+        WebElement requiredElement=element.get(1);
+        requiredElement.click();
+        WebElement capthaElement=driver.findElement(By.name("rfqSearchViewForm"));
+        WebElement capthaCode=capthaElement.findElement(By.tagName("i"));
+        String captha= capthaCode.getText();
+        WebElement element1=driver.findElement(By.name("inputCaptchaValue"));
+        element1.sendKeys(captha);
+        WebElement submitCapcha=driver.findElement(By.className("epsSubmit"));
+        submitCapcha.click();
+
+
     }
 }
